b8625bab5f550de290dc7b00084ccc05139335f3
GGonzalo <gonzaloacd@outlook.com>
2026-01-20 18:19:05 -0500

    feat: implement roles and permissions using Spatie

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('public.home');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return redirect()->route('admin.dashboard');
-    })->name('dashboard');
-
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/dashboard', fn () => redirect()->route('admin.dashboard'))->name('dashboard');
     Route::get('/admin', [DashboardController::class, 'index'])->name('admin.dashboard');
 });
 
